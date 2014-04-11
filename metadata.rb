09a7c83fd6d757096adb75f197962e326f06ad32
Darron Froese <darron@froese.org>
2014-04-11 11:39:17 -0600

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+name             'openresty'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures openresty'
+version          '0.1.0'
+recipe           'openresty::default', 'Installs/configures openresty'
+
+depends 'chef-sugar'
